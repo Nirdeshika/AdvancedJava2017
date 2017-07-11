a3ba440a0f5902e540f8cd6155d47a0f206f692a
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-11 13:36:01 -0700

    Added comments to Airline,Flight and Exception classes

--- src/main/java/edu/pdx/cs410J/np4/Flight.java
+++ src/main/java/edu/pdx/cs410J/np4/Flight.java
@@ -2,48 +2,112 @@ package edu.pdx.cs410J.np4;
 
 import edu.pdx.cs410J.AbstractFlight;
 
+/**
+ * This class represents a concrete flight class. It has a unique identifying number, a source and a destination airport identified by a three-letter code,
+ * a departure date and time, and an arrival date and time.
+ *
+ * @author Nirdeshika Polisetti
+ * @version 1.0
+ */
 public class Flight extends AbstractFlight {
 
-  private int number = 0;
-  private String source = null;
-  private String destination = null;
-  private String departDate = null;
-  private String departTime = null;
-  private String arrivalDate = null;
-  private String arrivalTime = null;
+    /**
+     * Unique identifying number
+     */
+    private int number = 0;
+    /**
+     * Source airport's code
+     */
+    private String source = null;
+    /**
+     * Destination airport's code
+     */
+    private String destination = null;
+    /**
+     * Departure date
+     */
+    private String departDate = null;
+    /**
+     * Departure time
+     */
+    private String departTime = null;
+    /**
+     * Arrival date
+     */
+    private String arrivalDate = null;
+    /**
+     * Arrival time
+     */
+    private String arrivalTime = null;
 
-  public Flight(int number, String source, String destination, String departDate, String departTime, String arrivalDate, String arrivalTime) {
-    this.number = number;
-    this.source = source;
-    this.destination = destination;
-    this.departDate = departDate;
-    this.departTime = departTime;
-    this.arrivalDate = arrivalDate;
-    this.arrivalTime = arrivalTime;
-  }
+    /**
+     * Create a flight object with the variables set to their corresponding values
+     *
+     * @param number      Unique identifying number
+     * @param source      Source airport's three letter code
+     * @param destination Destination airport's three letter code
+     * @param departDate  Departure date
+     * @param departTime  Departure time
+     * @param arrivalDate Arrival date
+     * @param arrivalTime Arrival time
+     */
+    public Flight(int number, String source, String destination, String departDate, String departTime, String arrivalDate, String arrivalTime) {
+        this.number = number;
+        this.source = source;
+        this.destination = destination;
+        this.departDate = departDate;
+        this.departTime = departTime;
+        this.arrivalDate = arrivalDate;
+        this.arrivalTime = arrivalTime;
+    }
 
-  @Override
-  public int getNumber() {
-    return number;
-  }
+    /**
+     * Returns the flight's unique identifying number
+     *
+     * @return Unique identifying number
+     */
+    @Override
+    public int getNumber() {
+        return number;
+    }
 
-  @Override
-  public String getSource() {
-    return source;
-  }
+    /**
+     * Returns the source airport's code
+     *
+     * @return A three lettered airport code for the source.
+     */
+    @Override
+    public String getSource() {
+        return source;
+    }
 
-  @Override
-  public String getDepartureString() {
-    return departDate + " " + departTime;
-  }
+    /**
+     * Concatenates departure date and time.
+     *
+     * @return Returns departure date and time as a single string
+     */
+    @Override
+    public String getDepartureString() {
+        return departDate + " " + departTime;
+    }
 
-  @Override
-  public String getDestination() {
-    return destination;
-  }
+    /**
+     * Returns the destination airport's code
+     *
+     * @return A three lettered airport code for the destination.
+     */
+    @Override
+    public String getDestination() {
+        return destination;
+    }
 
-  @Override
-  public String getArrivalString() {
-    return arrivalDate + " " + arrivalTime;
-  }
+    /**
+     * Concatenates arrival date and time.
+     *
+     * @return Returns arrival date and time as a single string
+     */
+    @Override
+    public String getArrivalString() {
+        return arrivalDate + " " + arrivalTime;
+    }
 }
--- src/main/java/edu/pdx/cs410J/np4/IllegalAirportCodeException.java
+++ src/main/java/edu/pdx/cs410J/np4/IllegalAirportCodeException.java
@@ -1,8 +1,18 @@
 package edu.pdx.cs410J.np4;
 
+/**
+ * An unchecked exception used to throw an exception when the format of the airport does not meet the requirements.
+ *
+ * @author Nirdeshika Polisetti
+ * @version 1.0
+ */
 public class IllegalAirportCodeException extends RuntimeException {
-    public IllegalAirportCodeException(String message)
-    {
+    /**
+     * Create an IllegalAirportCodeException object with the input message.
+     *
+     * @param message The message to be displayed when the exception occurs.
+     */
+    public IllegalAirportCodeException(String message) {
         super(message);
     }
 }
