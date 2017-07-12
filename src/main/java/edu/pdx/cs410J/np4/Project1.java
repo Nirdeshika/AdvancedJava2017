09f5bee3d27c89faf2ffae29e458b90bcea0dcd3
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-12 09:21:48 -0700

    Modified the project to add functionality 1. To check the validity of options. 2. To check if the flight number is a positive integer.

--- src/main/java/edu/pdx/cs410J/np4/Project1.java
+++ src/main/java/edu/pdx/cs410J/np4/Project1.java
@@ -4,20 +4,23 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 
 /**
  * The main class for the CS410J airline Project.
+ *
  * @author Nirdeshika Polisetti
  */
 public class Project1 {
 
     /**
-     *<p>
-     *     The entry point of the project. This method parses the command line arguments and checks for the correct type and format. If any of the
+     * <p>
+     * The entry point of the project. This method parses the command line arguments and checks for the correct type and format. If any of the
      * arguments does not match the requirements, the programs throws an exception and exits gracefully. If everything passes, then it sets
      * the appropriate variables with the given values.
      * </p>
+     *
      * @param args It is an array of Strings. It can take any number of Strings. But for the program to work as expected, it should be passed
      *             zero or more of the these two options: -README and -print and 8 other arguments. For more details, refer
      *             <a href = "http://web.cecs.pdx.edu/~whitlock/pdf/AppClasses.pdf">Project1 Details</a>
@@ -41,6 +44,15 @@ public class Project1 {
             return;
         }
 
+        if (numberOfOptions != 0) {
+            try {
+                checkValidityOfOptions(options);
+            } catch (IllegalOptionException ioe) {
+                System.out.println(ioe.getMessage());
+                System.exit(9);
+            }
+        }
+
         try {
             checkNumberOfArguments(countOfArgs, numberOfOptions);
         } catch (ErroneousNumberOfArgumentsException mae) {
@@ -49,12 +61,7 @@ public class Project1 {
         }
 
         nameOfTheAirLine = args[0 + numberOfOptions];
-        try {
-            flightNumber = Integer.parseInt(args[1 + numberOfOptions]);
-        } catch (NumberFormatException nfe) {
-            System.out.println("Invalid number: " + args[1 + numberOfOptions] + ". Please enter a valid flight number.");
-            System.exit(2);
-        }
+        flightNumber = getFlightNumber(args[1 + numberOfOptions]);
 
         try {
             checkAirportCodeFormat(args[2 + numberOfOptions]);
@@ -116,8 +123,34 @@ public class Project1 {
     }
 
     /**
+     * Returns the flight number after validating it. If it is not a number or if it is a negative integer, it throws an error.
+     *
+     * @param flightNumberAsString The command line argument corresponding to flight number.
+     * @return The flight number
+     * @throws NumberFormatException    if the passed argument is not a valid number.
+     * @throws IllegalArgumentException if the number is a negative integer.
+     */
+    private static int getFlightNumber(String flightNumberAsString) {
+        int flightNumber = 0;
+        try {
+            flightNumber = Integer.parseInt(flightNumberAsString);
+            if (flightNumber < 0) {
+                throw new IllegalArgumentException("Invalid flight number. Flight number should be a positive number.");
+            }
+        } catch (NumberFormatException nfe) {
+            System.out.println("Invalid number: " + flightNumberAsString + ". Flight number should be a positive integer.");
+            System.exit(2);
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+            System.exit(2);
+        }
+        return flightNumber;
+    }
+
+    /**
      * Returns a list of Strings containing the options passed to main method via command line.
-     * @param args The command line arguments passed to main method
+     *
+     * @param args        The command line arguments passed to main method
      * @param countOfArgs Count of number of command line arguments.
      * @return A list of Strings containing the options.
      * @see edu.pdx.cs410J.np4.Project1#main(String[])
@@ -135,8 +168,17 @@ public class Project1 {
         return options;
     }
 
+    private static void checkValidityOfOptions(ArrayList<String> options) {
+        if (!Arrays.asList("-README", "-print").containsAll(options)) {
+            throw new IllegalOptionException("Please check there is an invalid option. The valid options are -README and -print." +
+                    "Note that these are case sensitive.");
+        }
+
+    }
+
     /**
      * Returns the README of the project, a brief description of what the project does.
+     *
      * @return String containing the hard coded README.
      */
     private static String getReadMe() {
@@ -145,8 +187,8 @@ public class Project1 {
         readMe += "Purpose of the project: This project parses command line arguments to check for the validity of the values. " +
                 "If everything is of correct format and type, it creates an airline object and a flight object with the passed in arguments. " +
                 "It further adds the flight object to the airline object.\n" +
-                "This project also takes two options: -README prints a brief description of what the project does.\n" +
-                "\t\t\t\t     -print: Prints the details of the flight.\n";
+                "This project also takes two options: -README which prints a brief description of what the project does.\n" +
+                "\t\t\t\t     -print: Prints the details of the flight.\nNote that these are case-sensitive.";
         readMe += "Command Line Usage: java edu.pdx.cs410J.np4.Project1 [options] <args>\n" +
                 "args are (in order)\n" +
                 "name : Name of the flight : String\n" +
@@ -167,9 +209,10 @@ public class Project1 {
 
     /**
      * Checks if there are correct number of arguments required. If not, throws an exception.
-     * @param countOfArgs Number of command line arguments
+     *
+     * @param countOfArgs     Number of command line arguments
      * @param numberOfOptions Number of options passed in command line
-     * @exception ErroneousNumberOfArgumentsException
+     * @throws ErroneousNumberOfArgumentsException
      */
     private static void checkNumberOfArguments(int countOfArgs, int numberOfOptions) {
         if (countOfArgs - numberOfOptions < 8)
@@ -180,19 +223,21 @@ public class Project1 {
 
     /**
      * Checks if the input is string of three characters. If not, throws an exception.
+     *
      * @param airportCode A String of three lettered code for departure or arrival airport.
-     * @exception IllegalAirportCodeException
+     * @throws IllegalAirportCodeException
      */
     private static void checkAirportCodeFormat(String airportCode) {
         if (!airportCode.matches("[a-zA-z][a-zA-Z][a-zA-Z]"))
-            throw new IllegalAirportCodeException("Please check. Not a valid airport code: " + airportCode);
+            throw new IllegalAirportCodeException("Please check. Not a valid airport code: " + airportCode + ". Airport code should be a three lettered string.");
     }
 
     /**
      * Checks if the input String is of the format mm/dd/yyyy. Month and day can be 1 or digits, but year should be of 4 digits.
      * It also checks if the input is a valid date. If not, throws an exception.
+     *
      * @param date Departure date or Arrival date
-     * @exception ErroneousDateTimeFormatException
+     * @throws ErroneousDateTimeFormatException
      */
     private static void checkDateFormat(String date) {
         if (!date.matches("\\d{1,2}/\\d{1,2}/\\d{4}"))
@@ -210,8 +255,9 @@ public class Project1 {
     /**
      * Checks if the input string is of the format HH:mm. Hours can be 1 or 2 digits, but minutes should be 2 digits.
      * It also checks if it is a valid time. If not, it throws an exception.
+     *
      * @param time
-     * @exception ErroneousDateTimeFormatException
+     * @throws ErroneousDateTimeFormatException
      */
     private static void checkTimeFormat(String time) {
         if (!time.matches("\\d{1,2}:\\d{2}"))
