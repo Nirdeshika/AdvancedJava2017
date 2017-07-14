a734cb28e2a0754038507bad86cf4801f2c9a7a1
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-14 09:57:17 -0700

    Added a new exception class to throw an exception when the file format is not as required.

---
+++ src/main/java/edu/pdx/cs410J/np4/IllegalFileFormatException.java
@@ -0,0 +1,7 @@
+package edu.pdx.cs410J.np4;
+
+public class IllegalFileFormatException extends RuntimeException {
+    public IllegalFileFormatException(String message) {
+        super(message);
+    }
+}
