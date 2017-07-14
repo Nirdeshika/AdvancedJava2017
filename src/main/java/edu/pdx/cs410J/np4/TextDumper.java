fe2155093d0054dc94db5c85292ee734026c1a63
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-13 18:45:40 -0700

    TestDumper class It does the following: 1) Dumps to an existing file. 2) Dumps to a new file. 3) Throws an exception when dumped to a directory. 4) Throws an exception when dumped to a non existing directory.

---
+++ src/main/java/edu/pdx/cs410J/np4/TextDumper.java
@@ -0,0 +1,82 @@
+package edu.pdx.cs410J.np4;
+
+import edu.pdx.cs410J.AirlineDumper;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Nirdeshika Polisetti
+ */
+public class TextDumper implements AirlineDumper<Airline> {
+    private static String SEPARATOR = " || ";
+    private File file;
+
+    public TextDumper(String fileName) {
+        this.file = new File(fileName);
+    }
+
+    @Override
+    public void dump(Airline airline) {
+        BufferedWriter bufferedWriter;
+
+        checkIfFileExistsElseCreateIt();
+        checkIfItIsADirectory();
+
+        bufferedWriter = getBufferedWriter();
+        if (bufferedWriter != null) {
+            try {
+                Collection<Flight> flights;
+                flights = airline.getFlights();
+
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    if (bufferedReader.readLine() == null)
+                        bufferedWriter.write(airline.getName() + "\n");
+                }
+                for (Flight f : flights) {
+                    bufferedWriter.write(f.getNumber() + SEPARATOR + f.getSource() + SEPARATOR + f.getDepartureString() + SEPARATOR + f.getDestination() + SEPARATOR + f.getArrivalString()+"\n");
+                }
+
+                bufferedWriter.close();
+            } catch (IOException e) {
+                System.out.println("Cannot write to this file. " + e.getMessage());
+                System.exit(3);
+            }
+        }
+    }
+
+    private BufferedWriter getBufferedWriter() {
+        FileWriter fileWriter;
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            fileWriter = new FileWriter(file,true);
+            bufferedWriter = new BufferedWriter(fileWriter);
+        } catch (FileNotFoundException e) {
+            checkIfFileExistsElseCreateIt();
+        } catch (IOException e) {
+            System.out.println("Cannot write to this file. " + e.getMessage());
+            System.exit(3);
+        }
+        return bufferedWriter;
+    }
+
+    private void checkIfFileExistsElseCreateIt() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("File does not exist and cannot be created. "+ file +" : "+e.getMessage());
+                System.exit(1);
+            }
+        }
+    }
+
+    private void checkIfItIsADirectory() {
+        if (file.isDirectory()) {
+            System.out.println("A file is expected, but a directory is provided.");
+            System.exit(2);
+        }
+    }
+}
