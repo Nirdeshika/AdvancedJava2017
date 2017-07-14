22a4ab1684b3145d3fd3bb2accdc8f8c91316298
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-13 18:42:30 -0700

    TestDumper class's test class. It checks the following cases: 1) Dump to an existing file. 2) Dump to a new file. 3) Exception when dumped to a directory. 4) Exception when dumped to a non existing directory.

---
+++ src/test/java/edu/pdx/cs410J/np4/TextDumperTest.java
@@ -0,0 +1,45 @@
+package edu.pdx.cs410J.np4;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Unit tests for the {@link TextDumper} class.
+ *
+ * @author Nirdeshika Polisetti
+ */
+
+public class TextDumperTest {
+
+    private TextDumper getTextDumperObject(String filename) {
+        return new TextDumper(filename);
+    }
+
+
+    private Airline getAirlineObject() {
+        Airline airline = new Airline("Delta");
+        Flight flight = new Flight(1, "SEA", "PDX", "3/1/2017", "1:09", "3/1/2017", "2:09");
+        airline.addFlight(flight);
+        return airline;
+    }
+
+    @Test
+    public void dumpsIntoATxtFile() {
+        Airline airline = getAirlineObject();
+        getTextDumperObject("dumpingFile.txt").dump(airline);
+    }
+
+    @Test
+    public void dumpsWhenCalledWithDirectory()
+    {
+        getTextDumperObject("/Users/priyathamanisetty/airline/edu").dump(getAirlineObject());
+    }
+
+    @Test
+    public void dumpsWhenCalledWithNonExistingDirectory()
+    {
+        getTextDumperObject("/Users/priyathamanisetty/airline/nonExistingDirectory/edu").dump(getAirlineObject());
+    }
+
+}
