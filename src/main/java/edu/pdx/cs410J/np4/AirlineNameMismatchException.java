467abee00e2d72c505c0698e78bb6e159ce70e1f
Nirdeshika Polisetti <np4@pdx.edu>
2017-07-16 09:50:54 -0700

    A new exception class to throw an exception if the file name in the text file passed does not match with the file name in the command line.

---
+++ src/main/java/edu/pdx/cs410J/np4/AirlineNameMismatchException.java
@@ -0,0 +1,7 @@
+package edu.pdx.cs410J.np4;
+
+public class AirlineNameMismatchException extends RuntimeException {
+    public AirlineNameMismatchException(String message) {
+        super(message);
+    }
+}
